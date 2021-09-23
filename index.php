6606b766848cae30a21c6057e549969793e62d84
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-23 23:31:08 +0200

    array e foreach

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+require_once "partials/data.php"
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +11,11 @@
     <title>Google Faq</title>
 </head>
 <body>
-    
+    <header></header>    
+    <?php foreach ($faqs as $faq) { ?>
+        <h2><?php echo $faq["title"] ?></h2>
+        <?php foreach($faq["answers"] as $answer) echo $answer ?></p> 
+    <?php } ?>    
+    <footer></footer>
 </body>
 </html>
